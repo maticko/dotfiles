d1dc311c1464997949d4676c0bd0e26818e4ac12
Matic Lesjak <matic.lesjak@gmail.com>
2017-07-13 09:43:24 +1200

    add tree and nginx syntax highlighting to spacemacs

--- setup-script.sh
+++ setup-script.sh
@@ -11,7 +11,7 @@ echo "Installing homebrew cask"
 brew tap caskroom/cask
 
 # installing few basics
-brew install zsh zsh-completions wget shellcheck kubectl awscli emacs-plus z sshuttle watch zsh-syntax-highlighting
+brew install zsh zsh-completions wget shellcheck kubectl awscli emacs-plus z sshuttle watch zsh-syntax-highlighting tree
 brew linkapps emacs-plus
 
 # installing all programs that I need
